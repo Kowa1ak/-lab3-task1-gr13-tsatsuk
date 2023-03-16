551f514eefa0c9999542adfecae97dd22b09986c
Kowalak <89776358+Kowa1ak@users.noreply.github.com>
2023-03-16 18:34:31 +0000

    add comment

--- src/main.c
+++ src/main.c
@@ -1,8 +1,9 @@
-/* hello.c */
+//
+// created by danila tsatsuk
+//
 #include <stdio.h>
 
-void main (void)
-{
-	printf ("Hello World\n");
-	printf ("Goodbye World\n");
+void main(void) {
+  printf("Hello World\n");
+  printf("Goodbye World\n");
 }
